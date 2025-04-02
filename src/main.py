51ea0f576e7873e23c5726847d961354426412ff
Felix Winkelmann <felix.winkelmann02@gmail.com>
2025-04-02 20:35:59 +0200

    added adjustable grid for humans, human_grid_size

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from algorithms import run_a_star
 
 def test_warehouse_env():
     # Create warehouse environment with rendering
-    warehouse_env = env(grid_size=(34, 32), n_agents=8, n_humans=1, num_shelves=2048, num_pickup_points=3,
+    warehouse_env = env(grid_size=(34, 32), human_grid_size=(34, 32), n_agents=1, n_humans=1, num_shelves=2048, num_pickup_points=3,
                         num_dropoff_points=2, render_mode="human")
 
     run_a_star(warehouse_env, n_steps=1000)
